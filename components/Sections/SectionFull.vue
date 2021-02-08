52b12dc7a0e246dd91f687cbcd9de722de6fd7e6
Mahmoud ElWatidy <mahmoud.watidy@gmail.com>
2021-02-08 03:16:52 +0200

    initial commit

---
+++ components/Sections/SectionFull.vue
@@ -0,0 +1,18 @@
+<template>
+    <section :class="['flex flex-col md:flex-row w-full h-full', ...classes]">
+        <slot></slot>
+    </section> 
+</template>
+
+<script>
+export default {
+    props: ['classes'],
+}
+</script>
+
+<style>
+    section {
+        min-width: 100vw; 
+        min-height: 100vh; 
+    }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Tajawal:wght@200&display=swap');
+  
+  .font-tajawal {
+    font-family: 'Tajawal', sans-serif;
+  }
+  
+</style>
+<script>
+  export default {
+    head () {
+      return {
+          links: [
+            { 
+              href: "https://fonts.googleapis.com/css2?family=Tajawal:wght@200&display=swap"
+            },
+            {
+              href: "https://fonts.gstatic.com",
+              rel:"preconnect"
+            }
+          ]
+        }
+      }
+  }
+</script>
